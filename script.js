97d00c9abfb70c47e91d9a27ce4413cd06a6db95
dominantitan <kingaadit3@gmail.com>
2026-01-19 23:23:45 +0545

    Add button to change the number of grid elements

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 const container = document.querySelector("#container");
-const GRID_HEIGHT = 16;
-const GRID_WIDTH = 16;
+const sizebutton = document.querySelector("#sizebutton");
+let size = 16;
+const containerWidth = 500;
+let gridboxWidth;
+let Valid = true;
+let input;
 
-for (let j = 0; j < GRID_HEIGHT; j++) {
-  for (let i = 0; i < GRID_WIDTH; i++) {
-    const gridbox = document.createElement("div");
-    gridbox.classList.add("gridbox");
-    container.appendChild(gridbox);
-    console.log("1 grid box is added");
+function formGrid(length) {
+  for (let j = 0; j < length; j++) {
+    for (let i = 0; i < length; i++) {
+      const gridbox = document.createElement("div");
+      gridbox.classList.add("gridbox");
+      container.appendChild(gridbox);
+      //console.log("1 grid box is added");
+    }
   }
-  const divider = document.createElement("div");
-  divider.classList.add("divider");
-  container.appendChild(divider);
 }
+
+function removeGrid(nodeList) {
+  nodeList.forEach((element) => {
+    element.remove();
+  });
+}
+
+formGrid(size);
+let grid = document.querySelectorAll(".gridbox");
+
+sizebutton.addEventListener("click", () => {
+  do {
+    input = +prompt("give a size below 100");
+    if (input <= 100 && input != "") {
+      Valid = false;
+      removeGrid(grid);
+      size = input;
+      formGrid(size);
+      grid = document.querySelectorAll(".gridbox");
+      grid.forEach((element) => {
+        gridboxWidth = containerWidth / size - 1;
+        element.style.width = `${gridboxWidth}px`;
+      });
+    } else {
+      Valid = true;
+    }
+  } while (Valid);
+});
+
+grid.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    item.style.backgroundColor = "grey";
+  });
+});
+
+grid.forEach((item) => {
+  item.addEventListener("mouseleave", () => {
+    item.style.backgroundColor = "black";
+  });
+});
